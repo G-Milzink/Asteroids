7aea72be6c7e151909cd0eff8fee895224613c5e
G.Milzink <g.milzink@gmail.com>
2024-09-26 10:44:04 +0200

    clean up

--- asteroids/src/main/java/com/asteroids/App.java
+++ asteroids/src/main/java/com/asteroids/App.java
@@ -5,42 +5,32 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-
-
 public class App extends Application {
 
     private static Scene view;
-    private static Pane  canvas;
+    private static Pane canvas;
     private static int screenWidth = 800;
     private static int screenHeight = 600;
 
     @Override
     public void start(Stage window) throws Exception {
-        //Setup main view:
+        // Setup main view:
         canvas = new Pane();
         canvas.setPrefSize(screenWidth, screenHeight);
         canvas.setStyle("-fx-background-color: white;");
-        //Initialize player:
-        Ship player = new Ship(screenWidth/2, screenHeight/2);
-        //Initialize an asteroid:
+        // Initialize player:
+        Ship player = new Ship(screenWidth / 2, screenHeight / 2);
+        // Initialize an asteroid:
         Asteroid asteroid = new Asteroid(50, 50);
-
-
 
         canvas.getChildren().add(player.getEntity());
         canvas.getChildren().add(asteroid.getEntity());
 
-
-
-
-
-        //Assign view to window and show window:
+        // Assign view to window and show window:
         view = new Scene(canvas);
         window.setScene(view);
         window.show();
     }
-
-
 
     public static void main(String[] args) {
         launch();
