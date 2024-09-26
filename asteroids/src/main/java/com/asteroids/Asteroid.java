2ffd832fc318375083416ab06b36769c1a792916
G.Milzink <g.milzink@gmail.com>
2024-09-26 13:49:16 +0200

    randomize asteroid movement

--- asteroids/src/main/java/com/asteroids/Asteroid.java
+++ asteroids/src/main/java/com/asteroids/Asteroid.java
@@ -1,9 +1,31 @@
 package com.asteroids;
 
+import java.util.Random;
+
 public class Asteroid extends Entity {
+
+    private double rotationalMovement;
 
     public Asteroid(int x, int y) {
         super(new PolygonFactory().createPolygon(), x, y);
+
+        Random rnd = new Random();
+
+        super.getEntity().setRotate(rnd.nextInt(360));
+
+        int accelerationAmount = 1 + rnd.nextInt(10);
+        for (int i = 0; i < accelerationAmount; i++) {
+            accelerate();
+        }
+
+        this.rotationalMovement = 0.5 - rnd.nextDouble();
+
+    }
+
+    @Override
+    public void move() {
+        super.move();
+        super.getEntity().setRotate(super.getEntity().getRotate()+rotationalMovement);
     }
 
 }
