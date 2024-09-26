3547c81fdafa5a2266fe813eb121d904e1ff57c6
G.Milzink <g.milzink@gmail.com>
2024-09-26 10:30:29 +0200

    Create ship class

---
+++ asteroids/src/main/java/com/asteroids/Ship.java
@@ -0,0 +1,11 @@
+package com.asteroids;
+
+import javafx.scene.shape.Polygon;
+
+public class Ship extends Entity {
+
+    public Ship(int x, int y) {
+        super(new Polygon(-5,-5,10,0,-5,5), x, y);
+    }
+
+}
