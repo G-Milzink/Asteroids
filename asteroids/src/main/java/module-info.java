cb6963e56e071a027445b48be6670c912ee3c48b
G.Milzink <g.milzink@gmail.com>
2024-09-27 13:30:29 +0200

    add audio files to filestructure

--- asteroids/src/main/java/module-info.java
+++ asteroids/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.asteroids {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires java.desktop;
 
     opens com.asteroids to javafx.fxml;
     exports com.asteroids;
