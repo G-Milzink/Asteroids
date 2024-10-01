3f3eac9f81bfc464c3c2198d77f81245049a7b20
G.Milzink <g.milzink@gmail.com>
2024-10-01 15:02:53 +0200

    clean up

--- asteroids/src/main/java/com/asteroids/SFXSystem.java
+++ asteroids/src/main/java/com/asteroids/SFXSystem.java
@@ -70,6 +70,7 @@ public class SFXSystem {
             playerDeathSFX.start();
         }
     }
+    
     public void levelUpSound() {
         if (levelUpSFX != null) {
             levelUpSFX.stop();
