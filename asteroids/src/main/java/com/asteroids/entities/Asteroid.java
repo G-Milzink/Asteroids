9562220916c807ca0d08db59716e9435a8dcff0e
G.Milzink <g.milzink@gmail.com>
2024-10-04 11:18:44 +0200

    removed player/asteroid texturing

--- asteroids/src/main/java/com/asteroids/entities/Asteroid.java
+++ asteroids/src/main/java/com/asteroids/entities/Asteroid.java
@@ -4,23 +4,23 @@ import java.util.Random;
 
 import com.asteroids.utillities.PolygonFactory;
 
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
+// import javafx.scene.image.Image;
+// import javafx.scene.paint.ImagePattern;
 
 public class Asteroid extends Entity {
 
     private final double rotationalMovement;
-    private final Image image = new Image("file:asteroids/src/main/java/com/asteroids/img/asteroid_texture.jpg");
-    private final ImagePattern texture = new ImagePattern(image);
+    // private final Image image = new Image("file:asteroids/src/main/java/com/asteroids/img/asteroid_texture.jpg");
+    // private final ImagePattern texture = new ImagePattern(image);
 
     private final int baseSpeed;
 
     public Asteroid(int x, int y, int baseSpeed) {
         super(new PolygonFactory().createPolygon(), x, y);
-        this.getEntity().setFill(texture);
+        // this.getEntity().setFill(texture);
         this.getEntity().setStroke(Color.GREY);
-        this.getEntity().setStrokeWidth(1.0);
+        this.getEntity().setStrokeWidth(2.0);
         this.baseSpeed = baseSpeed;
 
         Random rnd = new Random();
--- asteroids/src/main/java/com/asteroids/entities/Ship.java
+++ asteroids/src/main/java/com/asteroids/entities/Ship.java
@@ -1,19 +1,19 @@
 package com.asteroids.entities;
 
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
+// import javafx.scene.image.Image;
+// import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Polygon;
 
 public class Ship extends Entity {
 
-    private final Image image = new Image("file:asteroids/src/main/java/com/asteroids/img/ship_texture.png");
-    private final ImagePattern texture = new ImagePattern(image);
+    // private final Image image = new Image("file:asteroids/src/main/java/com/asteroids/img/ship_texture.png");
+    // private final ImagePattern texture = new ImagePattern(image);
 
     public Ship(int x, int y) {
         super(new Polygon(-15, -15, 30, 0, -15, 15), x, y);
-        this.getEntity().setFill(texture);
-        this.getEntity().setStroke(Color.LIGHTSLATEGRAY);
+        // this.getEntity().setFill(texture);
+        this.getEntity().setStroke(Color.LIGHTBLUE);
         this.getEntity().setStrokeWidth(1.0);
     }
 
